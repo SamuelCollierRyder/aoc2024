beab5439cefbc90eb4b3f914fc0bc3faab094844
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2024-12-06 12:10:31 +0100

    Day 6 part 1 done

---
+++ day6/part1.py
@@ -0,0 +1,44 @@
+with open("input.txt") as f:
+    lines = f.readlines()
+    lines = [line.strip() for line in lines]
+
+
+current_pos = (0, 0)
+seen_positions = set()
+seen_positions_dir = set()
+current_direction = "up"
+possible_directions = ["up", "right", "down", "left"]
+
+for i in range(len(lines)):
+    if "^" in lines[i]:
+        current_pos = (i, lines[i].index("^"))
+        break
+
+print(current_pos)
+while True:
+    seen_positions.add(current_pos)
+    seen_positions_dir.add((current_pos, current_direction))
+    if current_direction == "up":
+        next_pos = (current_pos[0] - 1, current_pos[1])
+
+    elif current_direction == "right":
+        next_pos = (current_pos[0], current_pos[1]+ 1)
+
+    elif current_direction == "down":
+        next_pos = (current_pos[0] + 1, current_pos[1])
+
+    elif current_direction == "left":
+        next_pos = (current_pos[0], current_pos[1] - 1)
+
+    if next_pos[0] >= len(lines) or next_pos[1] >= len(lines[0]):
+        break
+
+    if lines[next_pos[0]][next_pos[1]] == "#":
+        current_direction = possible_directions[(possible_directions.index(current_direction) + 1) % 4]
+        continue
+
+    if (next_pos, current_direction) in seen_positions_dir:
+        break
+    current_pos = next_pos
+
+print(len(seen_positions))
