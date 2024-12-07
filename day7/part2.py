e0b19ebb92927c84196d54fdec41c49ade862c77
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2024-12-07 18:18:29 +0100

    Part 2 day 7 done

--- day7/part2.py
+++ day7/part2.py
@@ -1,17 +1,34 @@
+def int_to_base3(n):
+    if n == 0:
+        return '0'
+    
+    digits = []
+    while n:
+        n, remainder = divmod(n, 3)
+        digits.append(str(remainder))
+    
+    return ''.join(digits[::-1])
+
 def evaluate(numbers, operators):
     running_total = 0
     if operators[0] == "0":
         running_total = numbers[0] + numbers[1]
 
-    else:
+    elif operators[0] == "1":
         running_total = numbers[0] * numbers[1]
+
+    else:
+        running_total = int(str(numbers[0]) + str(numbers[1]))
 
     for i in range(2, len(numbers)):
         if operators[i-1] == "0":
             running_total += numbers[i]
 
-        else:
+        elif operators[i-1] == "1":
             running_total *= numbers[i]
+
+        else:
+            running_total = int(str(running_total) + str(numbers[i]))
 
     return running_total
 
@@ -25,16 +42,14 @@ for line in lines:
     numbers = line.split(":")[1]
     numbers = numbers.strip().split(" ")
     numbers = [int(number) for number in numbers]
-    for i in range(2**len(numbers)):
-        binary_str = bin(i) 
-        binary_str = binary_str[2:]
+    for i in range(3**len(numbers)):
+        base_three_str = int_to_base3(i)
         
-        if len(binary_str) <= len(numbers):
-            binary_str = "0" * (len(numbers) - len(binary_str)-1) + binary_str
+        if len(base_three_str) <= len(numbers):
+            base_three_str = "0" * (len(numbers) - len(base_three_str)-1) + base_three_str
 
-        if evaluate(numbers, list(binary_str)) == target:
+        if evaluate(numbers, list(base_three_str)) == target:
             s += target
             break
 
 print(s)
-
