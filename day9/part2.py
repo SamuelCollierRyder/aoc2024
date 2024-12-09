a67e5bfea8083f8acac9d02fdc677a1cc53bd038
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2024-12-09 13:01:37 +0100

    Trying new idea for part 2

--- day9/part2.py
+++ day9/part2.py
@@ -7,47 +7,20 @@ file = True
 file_count = 0
 for c in text:
     if file:
-        new_text += [str(file_count)] * int(c)
+        new_text += [str(file_count) * int(c)]
         file_count += 1
         file = False
 
     else:
-        new_text += ["."] * int(c)
+        new_text += ["." * int(c)]
         file = True
 
-print("".join(new_text))
-lp = 0
-up = len(new_text) - 1
-
-while up > 0:
-    while new_text[up] == ".":
-        up -= 1
-
-    data_size = 0
-    while new_text[up - data_size] == new_text[up]:
-        data_size += 1
-
-    while lp < len(new_text):
-        if new_text[lp] == ".":
-            space_size = 1
-            while new_text[lp + space_size] == ".":
-                space_size += 1
-
-            if data_size <= space_size:
-                new_text[lp: lp+data_size] = new_text[up-data_size+1:up+1]
-                new_text[up-data_size+1:up+1] = "." * data_size
-
-        else:
-            lp += 1
-    up -= data_size
+print(new_text)
 
 check_sum = 0
-found_dot = False
 for i in range(len(new_text)):
     if new_text[i] != ".":
         check_sum += int(new_text[i]) * (i)
-        # assert not found_dot
-    else:
-        found_dot = True
+
 print("".join(new_text))
 print(check_sum)
