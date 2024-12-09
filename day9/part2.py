ba0ddb9be3ab15fe14eaf9430fb8297cd5c6afcd
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2024-12-09 15:18:30 +0100

    Trying new idea for part 2

--- day9/part2.py
+++ day9/part2.py
@@ -7,20 +7,37 @@ file = True
 file_count = 0
 for c in text:
     if file:
-        new_text += [str(file_count) * int(c)]
+        if c != "0":
+            new_text.append(str(file_count) * int(c))
         file_count += 1
         file = False
 
     else:
-        new_text += ["." * int(c)]
+        if c != "0":
+            new_text.append("." * int(c))
         file = True
 
-print(new_text)
+lp = 0
+up = len(new_text) - 1
 
-check_sum = 0
-for i in range(len(new_text)):
-    if new_text[i] != ".":
-        check_sum += int(new_text[i]) * (i)
+while lp < up:
+    if "." not in new_text[lp]:
+        lp += 1
 
-print("".join(new_text))
-print(check_sum)
+    elif "." not in new_text[up]:
+        up -= 1
+
+    else:
+        if len(new_text[lp]) >= len(new_text[up]):
+            dot_len = len(new_text[lp])
+            new_text[lp] = new_text[up]
+            new_text[up] = "." * len(new_text[up])
+            if len(new_text[up]) < dot_len:
+                new_text.insert(lp+1, "." * (dot_len - len(new_text[up])))
+
+            up -= 1
+            lp += 1
+
+            print(new_text)
+            quit()
+
