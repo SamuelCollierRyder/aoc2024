203240fd49cd34f3886ce1a29a33c2bac386f9ab
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2024-12-09 15:20:00 +0100

    Trying new idea for part 2

--- day9/part2.py
+++ day9/part2.py
@@ -1,4 +1,4 @@
-with open("example_input.txt") as f:
+with open("input.txt") as f:
     text = f.read()
     text = text.strip()
 
@@ -7,37 +7,14 @@ file = True
 file_count = 0
 for c in text:
     if file:
-        if c != "0":
-            new_text.append(str(file_count) * int(c))
+        new_text += [str(file_count)] * int(c)
         file_count += 1
         file = False
 
     else:
-        if c != "0":
-            new_text.append("." * int(c))
+        new_text += ["."] * int(c)
         file = True
 
 lp = 0
 up = len(new_text) - 1
-
-while lp < up:
-    if "." not in new_text[lp]:
-        lp += 1
-
-    elif "." not in new_text[up]:
-        up -= 1
-
-    else:
-        if len(new_text[lp]) >= len(new_text[up]):
-            dot_len = len(new_text[lp])
-            new_text[lp] = new_text[up]
-            new_text[up] = "." * len(new_text[up])
-            if len(new_text[up]) < dot_len:
-                new_text.insert(lp+1, "." * (dot_len - len(new_text[up])))
-
-            up -= 1
-            lp += 1
-
-            print(new_text)
-            quit()
-
+print(new_text)
