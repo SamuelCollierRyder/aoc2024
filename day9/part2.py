baafa62b2ee58b71853f83dc4724cdb462180674
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2024-12-09 13:00:52 +0100

    Trying new idea for part 2

--- day9/part2.py
+++ day9/part2.py
@@ -18,30 +18,29 @@ for c in text:
 print("".join(new_text))
 lp = 0
 up = len(new_text) - 1
-while lp < up:
+
+while up > 0:
     while new_text[up] == ".":
         up -= 1
 
-    if new_text[lp] == ".":
-        space_size = 1
-        while new_text[lp + space_size] == ".":
-            space_size += 1
+    data_size = 0
+    while new_text[up - data_size] == new_text[up]:
+        data_size += 1
 
-        data_size = 0
-        while new_text[up - data_size] == new_text[up]:
-            print(new_text[up], data_size)
-            data_size += 1
+    while lp < len(new_text):
+        if new_text[lp] == ".":
+            space_size = 1
+            while new_text[lp + space_size] == ".":
+                space_size += 1
 
-        if data_size <= space_size:
-            new_text[lp: lp+data_size] = new_text[up-data_size+1:up+1]
-            new_text[up-data_size+1:up+1] = "." * data_size
+            if data_size <= space_size:
+                new_text[lp: lp+data_size] = new_text[up-data_size+1:up+1]
+                new_text[up-data_size+1:up+1] = "." * data_size
 
-        up -= data_size
+        else:
+            lp += 1
+    up -= data_size
 
-    else:
-        lp += 1
-
-print("".join(new_text))
 check_sum = 0
 found_dot = False
 for i in range(len(new_text)):
@@ -50,5 +49,5 @@ for i in range(len(new_text)):
         # assert not found_dot
     else:
         found_dot = True
-
+print("".join(new_text))
 print(check_sum)
