4ed93c76e30422db2d9a0e55108dd060c93f9ec3
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2024-12-09 12:30:10 +0100

    Done day 9 part one, still working on part 2

--- day9/part2.py
+++ day9/part2.py
@@ -1,4 +1,4 @@
-with open("input.txt") as f:
+with open("example_input.txt") as f:
     text = f.read()
     text = text.strip()
 
@@ -15,6 +15,7 @@ for c in text:
         new_text += ["."] * int(c)
         file = True
 
+print("".join(new_text))
 lp = 0
 up = len(new_text) - 1
 while lp < up:
@@ -22,38 +23,31 @@ while lp < up:
         up -= 1
 
     if new_text[lp] == ".":
-        new_text[lp] = new_text[up]
-        new_text[up] = "."
-        lp += 1
+        space_size = 1
+        while new_text[lp + space_size] == ".":
+            space_size += 1
+
+        data_size = 0
+        while new_text[up - data_size] == new_text[up]:
+            print(new_text[up], data_size)
+            data_size += 1
+
+        if data_size <= space_size:
+            new_text[lp: lp+data_size] = new_text[up-data_size+1:up+1]
+            new_text[up-data_size+1:up+1] = "." * data_size
+
+        up -= data_size
 
     else:
         lp += 1
 
-# For some reason I get an off by one error if I don't do 
-# a second pass
-lp = 0
-up = len(new_text) - 1
-while lp < up:
-    while new_text[up] == ".":
-        up -= 1
-
-    if new_text[lp] == ".":
-        new_text[lp] = new_text[up]
-        new_text[up] = "."
-        lp += 1
-
-    else:
-        lp += 1
-
-with open("output.txt", "w") as f:
-    f.write("".join(new_text))
-
+print("".join(new_text))
 check_sum = 0
 found_dot = False
 for i in range(len(new_text)):
     if new_text[i] != ".":
         check_sum += int(new_text[i]) * (i)
-        assert not found_dot
+        # assert not found_dot
     else:
         found_dot = True
 
